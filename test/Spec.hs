d68f385d5cc1113fe2a15c61df9ef31c5ab2f7a9
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-04-30 12:50:55 +0900

    Simplify the test spec by introducing a helper function shouldReturnRight

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,42 +3,46 @@ module Main where
 import Scheme
 import Test.Hspec
 
+infix 1 `shouldReturnRight`
+
+action `shouldReturnRight` expected = action >>= (`shouldBe` (Right expected))
+
 evalSpec :: Spec
 evalSpec =
   describe "evalString" $ do
     it "evaluates a string" $ do
       env <- newEnv
-      evalString env "\"foo\"" `shouldReturn` (Right $ String "foo")
+      evalString env "\"foo\"" `shouldReturnRight` String "foo"
 
     it "evaluates a positive number" $ do
       env <- newEnv
-      evalString env "123" `shouldReturn` (Right $ Number 123)
+      evalString env "123" `shouldReturnRight` Number 123
 
     it "evaluates a negative number" $ do
       env <- newEnv
-      evalString env "-123" `shouldReturn` (Right $ Number . negate $ 123)
+      evalString env "-123" `shouldReturnRight` (Number . negate $ 123)
 
     it "evaluates an atom" $ do
       env <- newEnv
-      evalString env "'foo" `shouldReturn` (Right $ Atom "foo")
+      evalString env "'foo" `shouldReturnRight` Atom "foo"
 
     it "evaluates a true value" $ do
       env <- newEnv
-      evalString env "#t" `shouldReturn` (Right $ Bool True)
+      evalString env "#t" `shouldReturnRight` Bool True
 
     it "evaluates a false value" $ do
       env <- newEnv
-      evalString env "#f" `shouldReturn` (Right $ Bool False)
+      evalString env "#f" `shouldReturnRight` Bool False
 
     it "evaluates begin expressions sequentially from left to right" $ do
       env <- newEnv
       evalString env "(begin (define x 0) (set! x 5) (+ x 1))"
-        `shouldReturn` (Right $ Number 6)
+        `shouldReturnRight` Number 6
 
     it "defines a variable" $ do
       env <- newEnv
-      evalString env "(define x 28)" `shouldReturn` (Right $ Number 28)
-      evalString env "x" `shouldReturn` (Right $ Number 28)
+      evalString env "(define x 28)" `shouldReturnRight` Number 28
+      evalString env "x" `shouldReturnRight` Number 28
 
 main = hspec $ do
   evalSpec
