1476c22c425514eb16cc255219fb93b41f25fac2
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-04-30 12:01:15 +0900

    Add more tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -11,20 +11,30 @@ evalSpec =
       res <- evalString env "\"foo\""
       res `shouldBe` (Right $ String "foo")
 
-    it "evaluates a number" $ do
+    it "evaluates a positive number" $ do
       env <- primitiveBindings
       res <- evalString env "123"
       res `shouldBe` (Right $ Number 123)
+
+    it "evaluates a negative number" $ do
+      env <- primitiveBindings
+      res <- evalString env "-123"
+      res `shouldBe` (Right $ Number . negate $ 123)
 
     it "evaluates an atom" $ do
       env <- primitiveBindings
       res <- evalString env "'foo"
       res `shouldBe` (Right $ Atom "foo")
 
-    it "evaluates a boolean value" $ do
+    it "evaluates a true value" $ do
       env <- primitiveBindings
       res <- evalString env "#t"
       res `shouldBe` (Right $ Bool True)
+
+    it "evaluates a false value" $ do
+      env <- primitiveBindings
+      res <- evalString env "#f"
+      res `shouldBe` (Right $ Bool False)
 
     it "evaluates begin expressions sequentially from left to right" $ do
       env <- primitiveBindings
