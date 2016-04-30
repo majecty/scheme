f5fce2ef9cdd5b4263995b5f978d8a6dec2b2157
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-05-01 01:22:20 +0900

    Add type signatures to numBoolBinop, strBoolBinop and boolBoolBinop

--- src/Scheme/Evaluator.hs
+++ src/Scheme/Evaluator.hs
@@ -122,8 +122,13 @@ boolBinop unpacker op args = if length args /= 2
                                      right <- unpacker $ args !! 1
                                      return $ Bool $ left `op` right
 
+numBoolBinop :: (Integer -> Integer -> Bool) -> [LispVal] -> ThrowsError LispVal
 numBoolBinop = boolBinop unpackNum
+
+strBoolBinop :: (String -> String -> Bool) -> [LispVal] -> ThrowsError LispVal
 strBoolBinop = boolBinop unpackStr
+
+boolBoolBinop :: (Bool -> Bool -> Bool) -> [LispVal] -> ThrowsError LispVal
 boolBoolBinop = boolBinop unpackBool
 
 unpackNum :: LispVal -> ThrowsError Integer
