d08ac398ac5bad93bb64194095bad222420031bd
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-04-30 12:16:31 +0900

    Use shouldReturn to simplify tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,38 +8,32 @@ evalSpec =
   describe "evalString" $ do
     it "evaluates a string" $ do
       env <- primitiveBindings
-      res <- evalString env "\"foo\""
-      res `shouldBe` (Right $ String "foo")
+      evalString env "\"foo\"" `shouldReturn` (Right $ String "foo")
 
     it "evaluates a positive number" $ do
       env <- primitiveBindings
-      res <- evalString env "123"
-      res `shouldBe` (Right $ Number 123)
+      evalString env "123" `shouldReturn` (Right $ Number 123)
 
     it "evaluates a negative number" $ do
       env <- primitiveBindings
-      res <- evalString env "-123"
-      res `shouldBe` (Right $ Number . negate $ 123)
+      evalString env "-123" `shouldReturn` (Right $ Number . negate $ 123)
 
     it "evaluates an atom" $ do
       env <- primitiveBindings
-      res <- evalString env "'foo"
-      res `shouldBe` (Right $ Atom "foo")
+      evalString env "'foo" `shouldReturn` (Right $ Atom "foo")
 
     it "evaluates a true value" $ do
       env <- primitiveBindings
-      res <- evalString env "#t"
-      res `shouldBe` (Right $ Bool True)
+      evalString env "#t" `shouldReturn` (Right $ Bool True)
 
     it "evaluates a false value" $ do
       env <- primitiveBindings
-      res <- evalString env "#f"
-      res `shouldBe` (Right $ Bool False)
+      evalString env "#f" `shouldReturn` (Right $ Bool False)
 
     it "evaluates begin expressions sequentially from left to right" $ do
       env <- primitiveBindings
-      res <- evalString env "(begin (define x 0) (set! x 5) (+ x 1))"
-      res `shouldBe` (Right $ Number 6)
+      evalString env "(begin (define x 0) (set! x 5) (+ x 1))"
+        `shouldReturn` (Right $ Number 6)
 
 main = hspec $ do
   evalSpec
