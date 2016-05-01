803f3d7233aecba34d3422d2bee6bf4988fc16b1
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-05-02 00:25:43 +0900

    Add list-tail procedure to stdlib.scm

--- stdlib.scm
+++ stdlib.scm
@@ -63,6 +63,10 @@
 (define (length lst)        (fold (lambda (x y) (+ x 1)) 0 lst))
 (define (append lst . lsts) (foldr (flip (curry foldr cons)) lst lsts))
 (define (reverse lst)       (fold (flip cons) '() lst))
+(define (list-tail x k)
+  (if (zero? k)
+    x
+    (list-tail (cdr x) (- k 1))))
 (define (mem-helper pred op) (lambda (acc next) (if (and (not acc) (pred (op next))) next acc)))
 (define (memq obj lst)       (fold (mem-helper (curry eq? obj) id) #f lst))
 (define (memv obj lst)       (fold (mem-helper (curry eqv? obj) id) #f lst))
