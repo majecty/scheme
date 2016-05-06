9d1245b527067ebe3131758d749ccd655b9d0fee
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-05-06 21:26:54 +0900

    Implement schemefmt

--- schemefmt/Main.hs
+++ schemefmt/Main.hs
@@ -1,5 +1,40 @@
 {-# LANGUAGE NamedFieldPuns #-}
 module Main where
 
+import Control.Monad.Except
+import Data.Foldable
+import System.Environment
+import System.IO
+
+import Language.Scheme.Parser
+import Language.Scheme.Pretty
+
+data FormatError = FormatError deriving (Eq, Show)
+type FormatM = ExceptT FormatError IO
+
+-- FIXME: Don't ignore blank lines
+formatString :: String -> FormatM String
+formatString s = do
+  case readExprList s of
+    Left _ -> throwError FormatError
+    Right exprs -> pure $ prettyPrint exprs
+
+-- FIXME: Add IO exception handling
+formatFile :: FilePath -> IO ()
+formatFile path = do
+  contents <- readFile path
+  res <- runExceptT (formatString contents)
+  case res of
+    Left e  -> print e
+    Right newContents -> writeFile path newContents
+
+usage :: String
+usage = "Usage: schemefmt [path ...]"
+
 main :: IO ()
-main = putStrLn "schemefmt"
+main = do
+  args <- getArgs
+  if null args
+     then putStrLn usage
+     else traverse_ formatFile args
+
